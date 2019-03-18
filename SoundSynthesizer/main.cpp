0b4cc4f41a7a54c55fa092761835a701de2c2517
webbuildermn <46059943+webbuildermn@users.noreply.github.com>
2019-03-18 18:18:24 -0500

    Added oscillator and envelopes to code per OneLoneCoder

--- SoundSynthesizer/main.cpp
+++ SoundSynthesizer/main.cpp
@@ -3,29 +3,139 @@ using namespace std;
 
 #include "olcNoiseMaker.h"
 
+double w(double dHertz)
+{
+	return dHertz * 2.0 * PI;
+}
 
-atomic<double> dFrequencyOutput = 0.0;
 
-double MakeNoise(double dTime)
+// Converts Frequency (Hz) to angular velocity
+
+
+double osc(double dHertz, double dTime, int nType)
 {
 
-	double dOutput = 1.0*sin(dFrequencyOutput * 2 * 3.14159 *dTime); 
+	switch (nType)
+	{
+	case 0: // sine wave
+		return sin(w(dHertz)* dTime);
+	case 1: // square wave
+		return sin(w(dHertz)* dTime) > 0.0 ? 1.0 : -1.0;
+	case 2: // triangle wave
+		return asin(sin(w(dHertz)* dTime)) * 2.0/PI;
+	case 3: // sawtooth wave (analogue, warm slow)
+	{
+		double dOutput = 0.0;
+		for (double n = 1.0; n < 10.0; n++)
+			dOutput += (sin(n*w(dHertz)*dTime)) / n;
+		return dOutput * (2.0 / PI);
+	}
+	case 4: // saw wave (optimized harsh fast)
+		return (2.0 / PI)*(dHertz*PI*fmod(dTime, 1.0 / dHertz) - (PI / 2.0));
+	case 5: //pseudo random noise
+		return 2.0* ((double)rand() / (double)RAND_MAX) - 1.0;
+	default: 
+		return 0.0;
+	}
+}
 
-	// return dOutput *.5; // sine wave toggle
+struct sEnvelopeADSR
+{
+	double dAttackTime;
+	double dDecayTime;
+	double dReleaseTime;
+	double dSustainAmplitude;
+	double dStartAmplitude;
 
-	// square wave toggle
-	if (dOutput > 0.0)
-		return 0.921;
-	else
-		return -0.921;
+	double dTriggerOnTime;
+	double dTriggerOffTime;
 
+	bool bNoteOn;
+
+	sEnvelopeADSR()
+	{
+		dAttackTime = 0.10;
+		dDecayTime = 0.05;
+		dStartAmplitude = 1.0;
+		dSustainAmplitude = .80;
+		dReleaseTime = 0.20;
+		dTriggerOffTime = 0.0;
+		dTriggerOffTime = 0.0;
+		bNoteOn = false;
+	}
+
+	double GetAmplitude(double dTime)
+	{
+		double dAmplitude = 0.0;
+		double dLifeTime = dTime - dTriggerOnTime;
+
+		if (bNoteOn)
+		{
+			// ADS
+
+			// Attack
+			if (dLifeTime <= dAttackTime)
+				dAmplitude = (dLifeTime / dAttackTime)* dStartAmplitude;
+
+			// Decay
+			if (dLifeTime > dAttackTime && dLifeTime <= (dAttackTime + dDecayTime))
+				dAmplitude = ((dLifeTime - dAttackTime) / dDecayTime)*(dSustainAmplitude - dStartAmplitude) + dStartAmplitude;
+			// sustain
+			if (dLifeTime > (dAttackTime + dDecayTime))
+				dAmplitude = dSustainAmplitude;
+		}
+		else
+		{
+			// Release
+			dAmplitude = ((dTime - dTriggerOffTime) / dReleaseTime)*(0.0 - dSustainAmplitude)+ dSustainAmplitude;
+		}
+
+		if (dAmplitude <= 0.0001)
+		{
+			dAmplitude = 0;
+		}
+
+		return dAmplitude;
+	};
+
+	// call when key is pressed
+	void NoteOn(double dTimeOn)
+	{
+		dTriggerOnTime = dTimeOn;
+		bNoteOn = true;
+	}
+
+	void NoteOff(double dTimeOff)
+	{
+		dTriggerOffTime = dTimeOff;
+		bNoteOn = false;
 	}
 
 
+};
+
+atomic<double> dFrequencyOutput = 0.0;
+sEnvelopeADSR envelope;
+double dOctaveBaseFrequency = 110.0; // A2  Frequency of octave represented by Hertz. Can toggle to higher value. 
+double d12thRootOf2 = pow(2.0, 1.0 / 12.0); // assuming western 12 notes/octave
+
+
+double MakeNoise(double dTime)
+{
+	double dOutput = envelope.GetAmplitude(dTime) * 
+		(
+			+ osc(dFrequencyOutput  *0.5, dTime, 3)
+			+ osc(dFrequencyOutput * 1.5, dTime, 1)
+
+		);  // Toggle 0,1,2,3,4
+
+	
+	return dOutput *.5; // Master Volume
+}
 
 int main()
 {
-    wcout << "Yiiiiii Haw Buddy. Let's play some Piano" << endl;
+    wcout << "Yiiiiii Haw. Let's play some Piano" << endl;
 
 	// Get all sound HW
 	vector<wstring> devices = olcNoiseMaker<short>::Enumerate();
@@ -48,52 +158,39 @@ int main()
 	// link noise function with sound machine
 	sound.SetUserFunction(MakeNoise);
 
-	double dOctaveBaseFrequency = 110.0; // A2
-	double d12thRootOf2 = pow(2.0, 1.0 / 12.0);
-	
-
+	int nCurrentKey = -1;
+	bool bKeyPressed = false;
 	while (1)
 	{
-		// add a keyboard like a piano
-
-		bool bKeyPressed = false;
+		bKeyPressed = false;
+		/*if (GetAsyncKeyState((unsigned char)("Q")) & 0x8000)
+	dOctaveBaseFrequency = 220.0;*/ // my failed code to raise the octave
 		for (int k = 0; k < 16; k++)
 		{
-			if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbcL\xbe\xbf"[k])) & 0x8000)
-			{
-				dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
-				bKeyPressed = true;
 
-			}
+				if (GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbcL\xbe\xbf"[k])) & 0x8000)
+				{					
+
+					if (nCurrentKey != k)
+					{
+					dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
+					envelope.NoteOn(sound.GetTime());
+					wcout << "\rNote On : " << sound.GetTime() << "s " << dFrequencyOutput << "Hz";
+					nCurrentKey = k;
+					}
+				bKeyPressed = true;
+				}
 		}
 
 		if (!bKeyPressed)
 		{
-			dFrequencyOutput = 0.0;
+			if (nCurrentKey != -1)
+			{
+				wcout << "\rNote Off: " << sound.GetTime() << "s                             ";
+				envelope.NoteOff(sound.GetTime());
+				nCurrentKey = -1;
+			}
 		}
-
-
-
 	}
-
-
-
-
-
-
-
-
-
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
